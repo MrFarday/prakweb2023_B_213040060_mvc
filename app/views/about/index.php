d18771119da521376294df457b2a3a164115095d
Mr Farday <mr.farday@MacBook-Pro-Mr.local>
2023-12-12 21:39:28 +0700

    eps 6 Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-<
-    <h1>about me!</h1>
+<div class="container">
+    <h1 class='mt-4'>about me!</h1>
+    <img src="<?= BASEURL; ?>/img/mj.jpg" alt="Muhammad Jauhari" width="100" class="rounded-circle shadow">
     <p>Hallo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya adalah <?= $data['pekerjaan']; ?></p>
+</div>
